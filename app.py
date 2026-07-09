b6129d06e5a3af6320f1c0a250762854be72ea40
aisyahicaaa <sekyashiball@gmail.com>
2026-07-09 23:20:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ import pickle
 from preprocessing import preprocessing
 
 # ==========================
+# KONFIGURASI HALAMAN
+# ==========================
+st.set_page_config(
+    page_title="Analisis Sentimen BPD Bali",
+    page_icon="🏦",
+    layout="centered"
+)
+
+# ==========================
+# MEMANGGIL CSS
+# ==========================
+with open("style.css") as css:
+    st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
+
+# ==========================
 # LOAD MODEL
 # ==========================
 with open("vectorizer.pkl", "rb") as file:
@@ -15,26 +30,17 @@ with open("model_naive_bayes.pkl", "rb") as file:
     model = pickle.load(file)
 
 # ==========================
-# PAGE CONFIG
+# CARD
 # ==========================
-st.set_page_config(
-    page_title="Analisis Sentimen BPD Bali",
-    page_icon="🏦",
-    layout="centered"
-)
+st.markdown('<div class="card">', unsafe_allow_html=True)
 
-# ==========================
-# MEMANGGIL CSS
-# ==========================
-with open("style.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# Logo
+col1, col2, col3 = st.columns([1,2,1])
 
-# ==========================
-# TAMPILAN
-# ==========================
+with col2:
+    st.image("logo-bank-bpd-bali.png", width=110)
 
-st.image("logo-bank-bpd-bali.png", width=90)
-
+# Judul
 st.markdown(
     "<h1>Analisis Sentimen</h1>",
     unsafe_allow_html=True
@@ -45,25 +51,28 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Input
 teks = st.text_area(
     "Masukkan Ulasan",
-    placeholder="Contoh: Aplikasi sangat membantu dan mudah digunakan.",
+    placeholder="Contoh : Aplikasi sangat membantu dan mudah digunakan.",
     height=180
 )
 
+# Tombol
 if st.button("Analisis Sentimen"):
 
     if teks.strip() == "":
-        st.warning("Silakan masukkan ulasan terlebih dahulu.")
+        st.warning("Masukkan ulasan terlebih dahulu.")
 
     else:
+
         # Preprocessing
         clean_text = preprocessing(teks)
 
-        # Vectorisasi
+        # Vectorizer
         vector = vectorizer.transform([clean_text])
 
-        # Seleksi fitur
+        # Chi Square
         vector = chi_selector.transform(vector)
 
         # Prediksi
@@ -71,10 +80,18 @@ if st.button("Analisis Sentimen"):
 
         st.markdown("---")
 
+        st.subheader("Hasil Analisis")
+
         if prediksi == 1:
+
             st.success("😊 Sentimen Positif")
+
         else:
+
             st.error("😞 Sentimen Negatif")
 
-        with st.expander("Hasil Preprocessing"):
+        with st.expander("Lihat Hasil Preprocessing"):
+
             st.write(clean_text)
+
+st.markdown("</div>", unsafe_allow_html=True)
